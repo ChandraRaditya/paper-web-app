88767252d1f14425c3bc0b26ad0261ae5588ee70
ChandraRaditya <chandra@refactory.id>
2023-10-15 22:19:55 +0700

    add benefit section

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
+import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/homePage.vue'
+import HomePage from '@/views/home-page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
